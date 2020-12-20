a44d7de10dc608eabc0211093946403b960b3229
Mayil Vahanan <mavah93@gmail.com>
2020-12-20 22:44:16 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Boot Application accessing Standalone ActiveMQ
+
+#Docker image
+
+docker run -p 61616:61616 -p 8161:8161 rmohr/activemq
+
+admin/admin
---
+++ src/main/java/com/mayil1993/messaging/standaloneactivemqexample/StandaloneActivemqExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mayil1993.messaging.standaloneactivemqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneActivemqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandaloneActivemqExampleApplication.class, args);
+	}
+}
